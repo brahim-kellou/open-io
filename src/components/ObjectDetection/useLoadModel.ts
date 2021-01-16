95bf99d900112a814dc9074d13b7924fc79add29
brahim-kellou <fb_kellou@esi.dz>
2021-01-16 04:39:54 +0100

    fix(model): update tfjs

--- src/components/ObjectDetection/useLoadModel.ts
+++ src/components/ObjectDetection/useLoadModel.ts
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import * as cocoSsd from '@tensorflow-models/coco-ssd'
+import '@tensorflow/tfjs-backend-cpu';
+import '@tensorflow/tfjs-backend-webgl';
 
 function useLoadModel() {
   const [model, setModel] = useState<any>();
