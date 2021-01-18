787c76151405bf35a8af2e3d038df7b3e774eeeb
brahim-kellou <fb_kellou@esi.dz>
2021-01-18 23:55:12 +0100

    chore: add chakra-ui

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { ChakraProvider } from "@chakra-ui/react"
 import './App.css';
 import HomeView from '../views/HomeView';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <HomeView />
-      </header>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <header className="App-header">
+          <HomeView />
+        </header>
+      </div>
+    </ChakraProvider>
   );
 }
 
