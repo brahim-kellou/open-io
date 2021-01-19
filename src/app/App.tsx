a493158232de03c5be787adbcc917ab192e2a733
brahim-kellou <fb_kellou@esi.dz>
2021-01-19 01:57:47 +0100

    feat: handle style & count person

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import { ChakraProvider } from "@chakra-ui/react"
-import './App.css';
 import HomeView from '../views/HomeView';
 
 function App() {
   return (
     <ChakraProvider>
-      <div className="App">
-        <header className="App-header">
-          <HomeView />
-        </header>
-      </div>
+      <HomeView />
     </ChakraProvider>
   );
 }
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,40 @@
 import * as React from 'react';
+import { Grid, GridItem, Box, } from "@chakra-ui/react"
 import ObjectDetection from '../ObjectDetection';
+import Menu from '../Menu';
+import '../styles.css';
 
 const Home = () => {
+  const [countPerson, setCountPerson] = React.useState<number>(0);
+  const onDetection = (objects: any[]) => {
+    const count = objects.filter(obj => obj.class === "person").length;
+    console.log(count);
+    setCountPerson(count);
+  }
   return (
-    <div>
-      <ObjectDetection
-        width={800}
-        height={450}
-      />
-    </div>
+    <Box className="home" width="100%" height="100vh">
+      <Grid
+        height="100%"
+        templateColumns="repeat(12, 1fr)"
+      >
+        <GridItem
+          colSpan={3}
+        >
+          <Menu
+            countPerson={countPerson}
+          />
+        </GridItem>
+        <GridItem
+          colSpan={9}
+        >
+          <ObjectDetection
+            width={800}
+            height={450}
+            onDetection={onDetection}
+          />
+        </GridItem>
+      </Grid>
+    </Box>
   )
 }
 
