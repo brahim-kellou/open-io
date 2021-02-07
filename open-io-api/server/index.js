995b5f419ad0fe0bc03bd01ccf9a190b76486dc0
Brahim Kellou <brahimkellou@outlook.com>
2021-02-07 01:18:23 +0100

    fix(open-io-api): add socket.io and fix post api

--- open-io-api/server/index.js
+++ open-io-api/server/index.js
@@ -9,14 +9,34 @@ const io = require('socket.io')(http, {
 });
 
 const bodyParser = require('body-parser');
-const apiRoutes = require('./api');
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use('/api', apiRoutes)
+
 app.get('/', (req, resp) => {
   resp.send('<h1>API pour OpenIO</h1>');
 });
+app.get('/api', (req, resp) => {
+  resp.send('Hello from OpenIO API')
+})
+app.post('/api', (req, resp) => {
+  try {
+    console.log('person detected!')
+    return resp.status(200).send({
+      success: true,
+      message: 'well received',
+    })
+  } catch {
+    console.log(`Error ${500}`)
+  }
+})
+
+// socket listners
+io.on('connection', socket => {
+  socket.on('join', () => {
+    console.log('arduino connected!')
+  })
+})
 
 http.listen(port, () => {
   console.log(`App listening on port: ${port}`);
