e971f133a408c860a798383c05ce894f98be34a8
Brahim Kellou <brahimkellou@outlook.com>
2021-02-07 03:37:59 +0100

    chore: add fetch

--- open-io-clients/detector.js
+++ open-io-clients/detector.js
@@ -1,33 +1,42 @@
 const { Board, Pin } = require("johnny-five");
+const fetch = require('node-fetch');
 
 url = 'http://192.168.78.78/api'
 
 // var board = new Board();
 
-var board = new Board({
-    repl: false,
-});
+// var board = new Board({
+//     repl: false,
+// });
 
 
-board.on("ready", function () {
-    //read from A0
-    var pin = new Pin("A0");
-    var presence = true;
+// board.on("ready", function () {
+//     //read from A0
+//     var pin = new Pin("A0");
+//     var presence = true;
 
-    // Envoi d'un message vers le serveur.
-    pin.read(function (error, value) {
-        if (value < 500 && !presence) {
-            fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                }
-            }).then(resp => resp.json())
-                .then(resp => console.log(resp))
+//     // Envoi d'un message vers le serveur.
+//     pin.read(function (error, value) {
+//         if (value < 500 && !presence) {
+//             fetch(url, {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-type': 'application/json'
+//                 }
+//             }).then(resp => resp.json())
+//                 .then(resp => console.log(resp))
 
-            presence = true;
-        } else if (value >= 500) {
-            presence = false;
-        }
-    });
-});
+//             presence = true;
+//         } else if (value >= 500) {
+//             presence = false;
+//         }
+//     });
+// });
+
+fetch(url, {
+    method: 'POST',
+    headers: {
+        'Content-type': 'application/json'
+    }
+}).then(resp => resp.json())
+    .then(resp => console.log(resp))
