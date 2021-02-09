cfc78b8ff569924b66ddeb2a5a5ed6e9ae6dab2c
Brahim Kellou <brahimkellou@outlook.com>
2021-02-09 20:35:18 +0100

    fix: add max persons api

--- open-io-api/server/index.js
+++ open-io-api/server/index.js
@@ -8,22 +8,51 @@ const io = require('socket.io')(http, {
   },
   path: '/socket'
 });
+const { OPEN_DOOR, CLOSE_DOOR, PLAY_ALERT, STOP_ALERT } = require('./constants');
 
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
+var maxPersons = 10;
+var totalPersons = 0;
+
 app.use(bodyParser.json());
 
 app.get('/', (req, resp) => {
-  resp.send('<h1>API pour OpenIO</h1>');
+  resp.send('<h1>Hello from OpenIO</h1>');
 });
-app.get('/api', (req, resp) => {
+
+app.get('/api/', (req, resp) => {
   resp.send('Hello from OpenIO API')
 })
-app.post('/api', (req, resp) => {
+
+app.post('/api/persons/max/', (req, resp) => {
   try {
-    console.log('person detected!')
-    io.sockets.emit('open-door', 'everyone');
+    maxPersons = req.body.max_persons
+    console.log(`Max persons modified to ${maxPersons}`)
+    return resp.status(200).send({
+      success: true,
+      message: 'Max persons updated',
+    })
+  } catch {
+    console.log(`Error ${500}`)
+  }
+})
+
+app.post('/api/persons/detection/', (req, resp) => {
+  try {
+    console.log('Person passed')
+    totalPersons += 1;
+    if (totalPersons > maxPersons) {
+      io.sockets.emit(CLOSE_DOOR, 'everyone');
+      io.sockets.emit(PLAY_ALERT, 'everyone');
+    } else if (totalPersons == maxPersons) {
+      io.sockets.emit(CLOSE_DOOR, 'everyone');
+    } else {
+      io.sockets.emit(OPEN_DOOR, 'everyone');
+      io.sockets.emit(STOP_ALERT, 'everyone');
+    }
+
     return resp.status(200).send({
       success: true,
       message: 'well received',
